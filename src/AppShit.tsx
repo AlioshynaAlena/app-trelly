f9ff7ac0666d7da4011c63e4a460c4c56425c986
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-10-08 11:27:56 +0300

    finish

--- src/AppShit.tsx
+++ src/AppShit.tsx
@@ -1,99 +1,99 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
-import type { ApiResponse, Task, TaskDetailResponse } from './types';
+// import { useEffect, useRef, useState } from 'react'
+// import './App.css'
+// import type { ApiResponse, Task, TaskDetailResponse } from './types';
 
-function AppShit() {
+// function AppShit() {
 
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [selectedTaskId, setSelectedTaskId] = useState<string | null>(null)
-  const [listQueryStatus, setListQueryStatus] = useState<'success' | 'loading'>('loading')
-  const [detailQueryStatus, setDetailQueryStatus] = useState<'pending' | 'success' | 'loading'>('pending')
-  const [selectedTask, setSelectedTask] = useState<TaskDetailResponse | null>(null)
-  const abortControllerRef = useRef<null | AbortController>(null)
-
-
-
-  useEffect(() => {
-    fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks', {
-      headers: {
-        'API-KEY': '',
-      },
-    })
-      .then((res) => res.json()as Promise<ApiResponse> )
-      .then((json) => {
-        console.log(json);
-        setTasks(json.data);
-        setListQueryStatus('success')
-      });
-  }, []);
+//   const [tasks, setTasks] = useState<Task[]>([]);
+//   const [selectedTaskId, setSelectedTaskId] = useState<string | null>(null)
+//   const [listQueryStatus, setListQueryStatus] = useState<'success' | 'loading'>('loading')
+//   const [detailQueryStatus, setDetailQueryStatus] = useState<'pending' | 'success' | 'loading'>('pending')
+//   const [selectedTask, setSelectedTask] = useState<TaskDetailResponse | null>(null)
+//   const abortControllerRef = useRef<null | AbortController>(null)
 
 
 
-  const handleSelectTaskClick = (taskId: string, boardId: string) => {
-  setSelectedTaskId(taskId);
-  setDetailQueryStatus('loading')
-
-
-abortControllerRef.current?.abort()
-abortControllerRef.current = new AbortController();
-
-
-    fetch('https://trelly.it-incubator.app/api/1.0/boards/' + boardId + '/tasks/' + taskId, { 
-      signal: abortControllerRef.current.signal,
-      headers: {
-        'API-KEY': '',
-      },
-    })
-    //мы делаем запрос, а затем (then), когда придут данные я скажу этому ответу превратиться в json формат
-      .then((res) => res.json() as Promise<TaskDetailResponse>)
-      //затем, еогда ты превратишься в json выведи мне данные (это уже будет JS объект, с которым мы в последующем что-то сможем делать)
-      .then((json) => {
-        console.log(json);
-        setSelectedTask(json);
-        setDetailQueryStatus('success')
-      });
-  }
-
-
-  return (
-    <div style={{'display': 'flex', 'gap': 50}}>
-      <div>
-    <ul> 
-      <h3>List</h3>
-
-      {
-        listQueryStatus === 'loading' && <p>Loading...</p> 
-      }
-
-      {tasks.map((task) => {
-        const color = task.id === selectedTaskId ? "red" : "green"
-    return (
-      <li key={task.id} style={{color: color}}>
-        <h4 onClick={() => handleSelectTaskClick(task.id, task.attributes.boardId)}>Title: {task.attributes.title}</h4>
-      </li>
-    );
-  })}
-    </ul>
-    </div>
-    <div>
-      <h3>Details</h3>
-
-      {detailQueryStatus === 'loading' && <p>Loading...</p>}
-
-      {detailQueryStatus === 'success' && selectedTask && <div>
-        <h4>Title: {selectedTask.data.attributes.title}</h4>
-        <h4>BoardId: {selectedTask.data.attributes.boardId}</h4>
-        <h4>Status: {selectedTask.data.attributes.status}</h4>
-        <h4>Priority: {selectedTask.data.attributes.priority}</h4>
-        <h4>Added ad: {selectedTask.data.attributes.addedAt}</h4>
-        <h4>Attachments Count:{selectedTask.data.attributes.attachmentsCount}</h4>
-        </div>}  
-    </div>
-    </div>
-  )
-}
+//   useEffect(() => {
+//     fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks', {
+//       headers: {
+//         'API-KEY': '',
+//       },
+//     })
+//       .then((res) => res.json()as Promise<ApiResponse> )
+//       .then((json) => {
+//         console.log(json);
+//         setTasks(json.data);
+//         setListQueryStatus('success')
+//       });
+//   }, []);
 
 
 
+//   const handleSelectTaskClick = (taskId: string, boardId: string) => {
+//   setSelectedTaskId(taskId);
+//   setDetailQueryStatus('loading')
 
-export default AppShit
+
+// abortControllerRef.current?.abort()
+// abortControllerRef.current = new AbortController();
+
+
+//     fetch('https://trelly.it-incubator.app/api/1.0/boards/' + boardId + '/tasks/' + taskId, { 
+//       signal: abortControllerRef.current.signal,
+//       headers: {
+//         'API-KEY': '',
+//       },
+//     })
+//     //мы делаем запрос, а затем (then), когда придут данные я скажу этому ответу превратиться в json формат
+//       .then((res) => res.json() as Promise<TaskDetailResponse>)
+//       //затем, еогда ты превратишься в json выведи мне данные (это уже будет JS объект, с которым мы в последующем что-то сможем делать)
+//       .then((json) => {
+//         console.log(json);
+//         setSelectedTask(json);
+//         setDetailQueryStatus('success')
+//       });
+//   }
+
+
+//   return (
+//     <div style={{'display': 'flex', 'gap': 50}}>
+//       <div>
+//     <ul> 
+//       <h3>List</h3>
+
+//       {
+//         listQueryStatus === 'loading' && <p>Loading...</p> 
+//       }
+
+//       {tasks.map((task) => {
+//         const color = task.id === selectedTaskId ? "red" : "green"
+//     return (
+//       <li key={task.id} style={{color: color}}>
+//         <h4 onClick={() => handleSelectTaskClick(task.id, task.attributes.boardId)}>Title: {task.attributes.title}</h4>
+//       </li>
+//     );
+//   })}
+//     </ul>
+//     </div>
+//     <div>
+//       <h3>Details</h3>
+
+//       {detailQueryStatus === 'loading' && <p>Loading...</p>}
+
+//       {detailQueryStatus === 'success' && selectedTask && <div>
+//         <h4>Title: {selectedTask.data.attributes.title}</h4>
+//         <h4>BoardId: {selectedTask.data.attributes.boardId}</h4>
+//         <h4>Status: {selectedTask.data.attributes.status}</h4>
+//         <h4>Priority: {selectedTask.data.attributes.priority}</h4>
+//         <h4>Added ad: {selectedTask.data.attributes.addedAt}</h4>
+//         <h4>Attachments Count:{selectedTask.data.attributes.attachmentsCount}</h4>
+//         </div>}  
+//     </div>
+//     </div>
+//   )
+// }
+
+
+
+
+// export default AppShit
--- src/shared/libs/router/Router.tsx
+++ src/shared/libs/router/Router.tsx
@@ -1,83 +1,83 @@
-import {createContext, useContext, useEffect} from "react";
-import React from "react";
+// import {createContext, useContext, useEffect} from "react";
+// import React from "react";
 
 
-const RouterContext = createContext<any>(null)
+// const RouterContext = createContext<any>(null)
 
-export const BrowserRouter = (props: any) => {
-    return <RouterContext.Provider value={{pattern: null}}>
-        {props.children}
-    </RouterContext.Provider>
-}
+// export const BrowserRouter = (props: any) => {
+//     return <RouterContext.Provider value={{pattern: null}}>
+//         {props.children}
+//     </RouterContext.Provider>
+// }
 
-export const Route = (props: any) => {
-   // lexical envirinment (1)  // lexical envirinment (2)
-    const [_, setVersion] = React.useState(1) //  1
-   const value = useContext(RouterContext);
+// export const Route = (props: any) => {
+//    // lexical envirinment (1)  // lexical envirinment (2)
+//     const [_, setVersion] = React.useState(1) //  1
+//    const value = useContext(RouterContext);
 
-    useEffect(() => {
-        const listener = (e: any) => {
-            setVersion(prevState => prevState + 1 )
-        }
+//     useEffect(() => {
+//         const listener = (e: any) => {
+//             setVersion(prevState => prevState + 1 )
+//         }
 
-        (window as any).navigation.addEventListener('navigate', listener);
+//         (window as any).navigation.addEventListener('navigate', listener);
 
-        return () => {
-            (window as any).navigation.removeEventListener('navigate', listener);
-        }
-    }, [ ])// замыкание
+//         return () => {
+//             (window as any).navigation.removeEventListener('navigate', listener);
+//         }
+//     }, [ ])// замыкание
 
-    const currentAddress = window.location.pathname;
+//     const currentAddress = window.location.pathname;
 
-    if (matchPath(currentAddress, props.path)) {
-        value.pattern = props.path
-        return props.element
-    } else {
-        return null
-    }
-}
+//     if (matchPath(currentAddress, props.path)) {
+//         value.pattern = props.path
+//         return props.element
+//     } else {
+//         return null
+//     }
+// }
 
-export const NavLink = (props:any) => {
-    const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        e.preventDefault()
+// export const NavLink = (props:any) => {
+//     const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+//         e.preventDefault()
 
-        window.history.pushState({}, '', props.to);
-    }
+//         window.history.pushState({}, '', props.to);
+//     }
 
-    return <a href={props.to} onClick={ handleClick }>{props.children}</a>
-}
+//     return <a href={props.to} onClick={ handleClick }>{props.children}</a>
+// }
 
 
-export const useParams = () => {
-    const currentAddress = window.location.pathname;
-    const value = useContext(RouterContext);
+// export const useParams = () => {
+//     const currentAddress = window.location.pathname;
+//     const value = useContext(RouterContext);
 
-    const currentAddressSegments = currentAddress.split('/')
-    const patternSegments = value.pattern.split('/')
+//     const currentAddressSegments = currentAddress.split('/')
+//     const patternSegments = value.pattern.split('/')
 
-    const result = {} as any
+//     const result = {} as any
 
-    for (let i = 0; i < currentAddressSegments.length; i++) {
-        const segmentFromPath = currentAddressSegments[i]
-        const segmentFromPattern = patternSegments[i]
+//     for (let i = 0; i < currentAddressSegments.length; i++) {
+//         const segmentFromPath = currentAddressSegments[i]
+//         const segmentFromPattern = patternSegments[i]
 
-        if (segmentFromPattern[0] === ':') {
-            result[segmentFromPattern.substring(1)] = segmentFromPath
-        }
+//         if (segmentFromPattern[0] === ':') {
+//             result[segmentFromPattern.substring(1)] = segmentFromPath
+//         }
 
-    }
+//     }
 
-    return result
-}
+//     return result
+// }
 
-function matchPath(currentAddress: any, pattern: any) {
-    const currentAddressSegments = currentAddress.split('/')
-    const patternSegments = pattern.split('/')
+// function matchPath(currentAddress: any, pattern: any) {
+//     const currentAddressSegments = currentAddress.split('/')
+//     const patternSegments = pattern.split('/')
 
-    if  (currentAddressSegments.length !== patternSegments.length) {
-        return false
-    } else {
-        return true
-    }
+//     if  (currentAddressSegments.length !== patternSegments.length) {
+//         return false
+//     } else {
+//         return true
+//     }
 
-}
+// }
